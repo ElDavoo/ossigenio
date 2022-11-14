c015ab69ea90042e22ea2095d151953ccac376eb
Antonio Solida <solida.antonio@gmail.com>
2022-11-14 14:36:10 +0100

    Added arduino code

---
+++ proto1/proto1.ino
@@ -0,0 +1,172 @@
+/*
+air-quality-monitor
+A monitor for air quality, made for IoT and 3D Intelligent Systems @ UniMORE AA 2022-2023.
+Please do not divulgate.
+*/
+
+// start BLE include section
+#include <Arduino.h>
+#include <SPI.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+#include "BluefruitConfig.h"
+#if SOFTWARE_SERIAL_AVAILABLE
+  #include <SoftwareSerial.h>
+#endif
+#define FACTORYRESET_ENABLE         1
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MODE_LED_BEHAVIOUR          "MODE"
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+// end BLE section
+
+// start DHT11 include section
+#include <dht_nonblocking.h>
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+static const int DHT_SENSOR_PIN = 5;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+// end DHT11 section
+
+// start FEEDBACK include section
+#define buttonPin 2
+int feedback = 0;
+// end FEEDBACK include section
+
+unsigned int startMillis;
+unsigned int currentMillis;
+const unsigned long period = 1000;
+//int val = 0; //not required here
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+void ble_setup(){
+  //while (!Serial);  // required for Flora & Micro (seems didn't required for Adafruit Feather 32u4 Bluefruit LE)
+  delay(500);
+
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit Command <-> Data Mode Example"));
+  Serial.println(F("------------------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) ){
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE ){
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  //Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  //Serial.println(F("Then Enter characters to send to Bluefruit"));
+  //Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  Serial.println(F("******************************"));
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) ){
+    // Change Mode LED Activity
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+  }
+
+  // Set module to DATA mode
+  Serial.println( F("Switching to DATA mode!") );
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+  Serial.println(F("******************************"));
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  //Serial.begin(9600);
+  ble_setup();
+  pinMode(buttonPin, INPUT);
+  //startMillis = millis();
+}
+
+/*
+ * Poll for a measurement, keeping the state machine alive.  Returns
+ * true if a measurement is available.
+ */
+static bool measure_environment( float *temperature, float *humidity ){
+  static unsigned long measurement_timestamp = millis( );
+
+  /* Measure once every four seconds. */
+  if( millis( ) - measurement_timestamp > period ){
+    if( dht_sensor.measure( temperature, humidity ) == true ){
+      measurement_timestamp = millis( );
+      return( true );
+    }
+  }
+
+  return( false );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float temperature;
+  float humidity;
+  float co2;
+
+  // read co2
+  co2=analogRead(A3);
+  
+  // read the state of the pushbutton value:
+  feedback = digitalRead(buttonPin);
+
+  /* Measure temperature and humidity.  If the functions returns
+     true, then a measurement is available. */
+  if( measure_environment( &temperature, &humidity ) == true ){
+    /*Serial.print( "T = " );
+    Serial.print( temperature, 1 );
+    Serial.print( " deg. C, H = " );
+    Serial.print( humidity, 1 );
+    Serial.print( "%, CO2 = " );
+    Serial.println( val );*/
+    ble.print( "T = " );
+    ble.print( temperature, 1 );
+    ble.print( " deg. C, H = " );
+    ble.print( humidity, 1 );
+    ble.print( "%, CO2 = " );
+    ble.println( co2, 1 );
+    /*
+    // TO BE EVALUATED
+    ble.write(0xff); //16 bit for start sequence
+		ble.write(0x04); //16 bit for number of paramenters sent
+		ble.write((int) temperature); //16 bit for temperature
+	  ble.write((int) humidity); //16 bit for humidity
+    ble.write((int) co2); //16 bit for co2
+    ble.write(feedback); //16 bit for feedback
+		ble.write(0xfe); //16 bit for stop sequence
+
+    feedback=0; //set to 0 after send positive feedback
+    */
+  }
+
+  //ble.print("ciao");
+}
