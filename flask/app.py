026e47fce4b1ef6944bbebd5929fa910decc60f1
David <github@davidepalma.it>
2022-11-15 10:25:31 +0100

    add DB connection

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+import os
 
+from flask import Flask, render_template
+import psycopg2
 app = Flask(__name__)
 
+
+# Check if DB_PASSWORD is set
+if 'DB_PASSWORD' not in os.environ:
+    print("DB_PASSWORD not set")
+    exit(1)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="iot",
+        user="iot",
+        password=os.environ['DB_PASSWORD'])
+    return conn
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Saluta Antonio'
 
+
+
+
+@app.route('/measurements/')
+def index():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM measurements;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return books
 
 if __name__ == '__main__':
     port = 5000
