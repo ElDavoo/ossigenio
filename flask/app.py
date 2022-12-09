5178eebafdca111fe9fff69e51790ecb582f41be
Antonio Solida <solida.antonio@gmail.com>
2022-12-09 11:15:03 +0100

    fix

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 import psycopg2
 app = Flask(__name__)
 
@@ -28,6 +28,18 @@ def showMain():
 def registrazione():
     return render_template('signup.html')
 
+@app.route('/signUp')
+def signUp():
+    # create user code will be here !!
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    # validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 @app.route('/measurements/')
 def index():
