642d43652d5cf568d723446e4e940e9d1ac3208d
Antonio Solida <solida.antonio@gmail.com>
2022-12-10 14:29:59 +0100

    Fixed other functions

--- proto1/serialProtocol.cpp
+++ proto1/serialProtocol.cpp
@@ -26,7 +26,7 @@ extern volatile uint8_t feedback;
 #define MODEL 1
 #define VERSION 1
 
-void getMsg0(int temp, int humidity, int raw_data){
+/*void getMsg0(int temp, int humidity, int raw_data){ //OLD VERSION
     char buffer[5];
     //ble.print(0xAA80); //AA, numero campi e tipo --- 0xAA80 = dec43648
     ble.write(0xAA);
@@ -49,9 +49,31 @@ void getMsg0(int temp, int humidity, int raw_data){
     ble.print(0xFFFF); //PLACEHOLDER per separare il valore di co2 dal crc --- 0xFFF = dec65535
     sprintf(buffer, "%0.5d", crc);
 	ble.print(buffer); 
+}*/
+
+void getMsg0(int temp, int humidity, int raw_data){
+    //char buffer[5];
+    //ble.print(0xAA80); //AA, numero campi e tipo --- 0xAA80 = dec43648
+    ble.write(0xAA);
+    ble.write(0x80);
+    ble.write((uint8_t) temp);
+    ble.write((uint8_t) humidity);
+    ble.write(highByte(raw_data));
+    ble.write(lowByte(raw_data));
+
+    uint8_t message[4];
+
+    message[0] = (uint8_t) 0xAA80;
+    message[1] = (uint8_t) temp;
+    message[2] = (uint8_t) humidity;
+    message[3] = (uint8_t) raw_data;
+
+    uint8_t crc = checksumCalculator(message,4);
+    ble.write(0xFF); //PLACEHOLDER per separare il valore di co2 dal crc --- 0xFFF = dec65535
+	ble.write(crc); 
 }
 
-/*void getMsg1(int temp, int humidity, int co2) {
+/*void getMsg1(int temp, int humidity, int co2) { //OLD VERSION
     char buffer[5];
     //ble.print(0xAA81); //AA, numero campi e tipo --- 0xAA81 = dec43649
     ble.write(0xAA);
@@ -77,20 +99,14 @@ void getMsg0(int temp, int humidity, int raw_data){
 }*/
 
 void getMsg1(int temp, int humidity, int co2) {
-    char buffer[5];
-    //ble.print(0xAA81); //AA, numero campi e tipo --- 0xAA81 = dec43649
-    ble.write(0xAA);
+    //char buffer[5];
+
+    ble.write(0xAA); //AA, numero campi e tipo --- 0xAA81 = dec43649
     ble.write(0x81);
-    //sprintf(buffer, "%0.5d", temp);
-	//ble.print((unsigned short int)temp, 1); 
     ble.write((uint8_t) temp);
-	//sprintf(buffer, "%0.5d", humidity);
     ble.write((uint8_t) humidity);
-	//ble.print((unsigned short int)humidity, 1); 
-    //sprintf(buffer, "%0.5d", co2);
     ble.write(highByte(co2));
     ble.write(lowByte(co2));
-	//ble.print((unsigned short int)co2, 2); 
 
     uint8_t message[4];
 
@@ -101,7 +117,6 @@ void getMsg1(int temp, int humidity, int co2) {
 
     uint8_t crc = checksumCalculator(message,4);
     ble.write(0xFF); //PLACEHOLDER per separare il valore di co2 dal crc --- 0xFFF = dec65535
-    //sprintf(buffer, "%0.5d", crc);
 	ble.write(crc); 
 }
 
@@ -110,6 +125,28 @@ void getMsg2(/*NOT YET IMPLEMENTED*/){
 }
 
 void getMsg3(){
+    //char buffer[5];
+    //ble.print(0xAA83); //questo valore qui è stampato come 0xAA83 nel monitor seriale
+    uint8_t battery = 0;
+    ble.write(0xAA);
+    ble.write(0x83);
+    ble.write((uint8_t) MODEL);
+    ble.write((uint8_t) VERSION);
+    ble.write(battery);
+
+    uint8_t message[4];
+
+    message[0] = (uint8_t) 0xAA83;
+    message[1] = (uint8_t) MODEL;
+    message[2] = (uint8_t) VERSION;
+    message[3] = (uint8_t) 0;
+
+    uint8_t crc = checksumCalculator(message,4); 
+    ble.write(0xFF);
+	ble.write(crc); 
+}
+
+/*void getMsg3(){ //OLD VERSION
     char buffer[5];
     //ble.print(0xAA83); //questo valore qui è stampato come 0xAA83 nel monitor seriale
     ble.write(0xAA);
@@ -132,9 +169,33 @@ void getMsg3(){
     ble.print(0xFFFF); //PLACEHOLDER per separare il valore di co2 dal crc
     sprintf(buffer, "%0.5d", crc);
 	ble.print(buffer); 
-}
+}*/
 
 void getMsg4(int temp, int humidity, int co2, uint8_t feedback) {
+    //char buffer[5];
+    //ble.print(0xAA81); //AA, numero campi e tipo --- 0xAA81 = dec43649
+    ble.write(0xAA);
+    ble.write(0x94);
+    ble.write((uint8_t) temp);
+    ble.write((uint8_t) humidity);
+    ble.write(highByte(co2));
+    ble.write(lowByte(co2));
+    ble.write(feedback);
+
+    uint8_t message[5];
+
+    message[0] = (uint8_t) 0xAA81;
+    message[1] = (uint8_t) temp;
+    message[2] = (uint8_t) humidity;
+    message[3] = (uint8_t) co2;
+    message[4] = (uint8_t) feedback;
+
+    uint8_t crc = checksumCalculator(message,5);
+    ble.write(0xFF); //PLACEHOLDER per separare il valore di co2 dal crc --- 0xFFF = dec65535
+	ble.write(crc); 
+}
+
+/*void getMsg4(int temp, int humidity, int co2, uint8_t feedback) { //OLD VERSION
     char buffer[5];
     //ble.print(0xAA81); //AA, numero campi e tipo --- 0xAA81 = dec43649
     ble.write(0xAA);
@@ -160,7 +221,7 @@ void getMsg4(int temp, int humidity, int co2, uint8_t feedback) {
     ble.print(0xFFFF); //PLACEHOLDER per separare il valore di co2 dal crc --- 0xFFF = dec65535
     sprintf(buffer, "%0.5d", crc);
 	ble.print(buffer); 
-}
+}*/
 
 uint8_t checksumCalculator(uint8_t *data, uint8_t length){
    uint8_t curr_crc = 0x0000;
