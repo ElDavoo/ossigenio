5e2877f8a022deba46e89bf6b0c3b29b7d74f01d
David <github@davidepalma.it>
2022-12-20 17:11:56 +0100

    fix bug in ble adv data

--- proto2/BleSerial.cpp
+++ proto2/BleSerial.cpp
@@ -153,13 +153,16 @@ void BleSerial::begin(const char *name,bool enable_led, int led_pin)
 	SetupSerialService();
 
 	const uint8_t* point = esp_bt_dev_get_address();
-	char ManufacturerData[8] = {0x75,0xf1}; //0xF175; id produttore preso a caso tra quelli liberi
-  	for (int i = 0; i < 6; i++) ManufacturerData[i+2] = point[i];
+	char ManufacturerData[9] = {0x75,0xf1}; //0xF175; id produttore preso a caso tra quelli liberi
+	for (int i = 0; i < 6; i++) ManufacturerData[i+2] = point[i];
+	// null-terminate string
+	ManufacturerData[8] = 0x00;
 	oAdvertisementData.setManufacturerData(ManufacturerData); //to add mac into manufacter data; thanks Apple! -.-"
 
 	pAdvertising = BLEDevice::getAdvertising();
 	pAdvertising->setAdvertisementData(oAdvertisementData);
-	pAdvertising->addServiceUUID(BLE_SERIAL_SERVICE_UUID);
+	// it doesn't fit anyway
+	// pAdvertising->addServiceUUID(BLE_SERIAL_SERVICE_UUID);
 	pAdvertising->setScanResponse(true);
 	pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
 	pAdvertising->setMinPreferred(0x12);
