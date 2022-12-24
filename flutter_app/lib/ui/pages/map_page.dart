0f1991900c5e2f64d378d0dad5032072da2d7466
David <github@davidepalma.it>
2022-12-24 18:15:20 +0100

    map: added current location marker

--- flutter_app/lib/ui/pages/map_page.dart
+++ flutter_app/lib/ui/pages/map_page.dart
@@ -2,8 +2,11 @@
 This class files implements a stateful widget which shows a map
  */
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapPage extends StatefulWidget {
@@ -14,19 +17,68 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  late CenterOnLocationUpdate _centerOnLocationUpdate;
+  late StreamController<double?> _centerCurrentLocationStreamController;
+
+  @override
+  void initState() {
+    super.initState();
+    _centerOnLocationUpdate = CenterOnLocationUpdate.never;
+    _centerCurrentLocationStreamController = StreamController<double?>();
+  }
+
+  @override
+  void dispose() {
+    _centerCurrentLocationStreamController.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
       options: MapOptions(
         center: LatLng(44.6291399, 10.9488126),
         zoom: 17.0,
+        onPositionChanged: (MapPosition position, bool hasGesture) {
+          if (hasGesture) {
+            setState(
+                  () => _centerOnLocationUpdate = CenterOnLocationUpdate.never,
+            );
+          }
+        },
       ),
+      nonRotatedChildren: [
+        Positioned(
+          right: 20,
+          bottom: 20,
+          child: FloatingActionButton(
+            onPressed: () {
+              // Automatically center the location marker on the map when location updated until user interact with the map.
+              setState(
+                    () => _centerOnLocationUpdate = CenterOnLocationUpdate.always,
+              );
+              // Center the location marker on the map and zoom the map to level 18.
+              _centerCurrentLocationStreamController.add(18);
+            },
+            child: const Icon(
+              Icons.my_location,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
       children: [
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'it.eldavo.aqm',
         ),
+        CurrentLocationLayer(
+          centerCurrentLocationStream:
+          _centerCurrentLocationStreamController.stream,
+          centerOnLocationUpdate: _centerOnLocationUpdate,
+        ),
       ],
     );
   }
 }
+
