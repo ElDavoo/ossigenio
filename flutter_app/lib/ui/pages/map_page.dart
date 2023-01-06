0a1097375646ccad7f59979050ba81bd4598a181
David <github@davidepalma.it>
2023-01-06 19:47:28 +0100

    map: hide popups on tap

--- flutter_app/lib/ui/pages/map_page.dart
+++ flutter_app/lib/ui/pages/map_page.dart
@@ -20,16 +20,35 @@ class MapPage extends StatefulWidget {
   _MapPageState createState() => _MapPageState();
 }
 
-class _MapPageState extends State<MapPage> with AutomaticKeepAliveClientMixin<MapPage> {
+class _MapPageState extends State<MapPage>
+    with AutomaticKeepAliveClientMixin<MapPage> {
   final PopupController _popupController = PopupController();
   late CenterOnLocationUpdate _centerOnLocationUpdate;
   late StreamController<double?> _centerCurrentLocationStreamController;
   List<Marker> _markers = [];
 
+  Widget popupBuilder(BuildContext context, marker) {
+    return Container(
+      width: 200,
+      height: 100,
+      color: Colors.green,
+      child: GestureDetector(
+        onTap: () {
+          // close the popup
+          _popupController.hideAllPopups();
+        },
+        child: Text(
+          'Container popup for marker at ${marker.point}',
+        ),
+      ),
+    );
+  }
+
   void refresh() {
-    AccountManager().getPlaces(
-        LatLng(GpsManager.position!.latitude, GpsManager.position!.longitude)
-    ).then((value) {
+    AccountManager()
+        .getPlaces(LatLng(
+            GpsManager.position!.latitude, GpsManager.position!.longitude))
+        .then((value) {
       onNearbyPlacesChanged(value);
     });
   }
@@ -59,10 +78,14 @@ class _MapPageState extends State<MapPage> with AutomaticKeepAliveClientMixin<Ma
     final popupState = PopupState.maybeOf(context, listen: false);
     LatLng center = LatLng(44.6291399, 10.9488126);
     if (GpsManager.position != null) {
-      center = LatLng(GpsManager.position!.latitude, GpsManager.position!.longitude);
+      center =
+          LatLng(GpsManager.position!.latitude, GpsManager.position!.longitude);
     }
     return FlutterMap(
       options: MapOptions(
+        onTap: (_, __) {
+          _popupController.hideAllPopups();
+        },
         center: center,
         minZoom: 9.0,
         zoom: 15.0,
@@ -72,7 +95,7 @@ class _MapPageState extends State<MapPage> with AutomaticKeepAliveClientMixin<Ma
         onPositionChanged: (MapPosition position, bool hasGesture) {
           if (hasGesture) {
             setState(
-                  () => _centerOnLocationUpdate = CenterOnLocationUpdate.never,
+              () => _centerOnLocationUpdate = CenterOnLocationUpdate.never,
             );
           }
         },
@@ -81,38 +104,26 @@ class _MapPageState extends State<MapPage> with AutomaticKeepAliveClientMixin<Ma
         Positioned(
           right: 20,
           bottom: 20,
-          child: FloatingActionButton(
-            onPressed: () {
-              // Automatically center the location marker on the map when location updated until user interact with the map.
-              setState(
-                    () => _centerOnLocationUpdate = CenterOnLocationUpdate.always,
-              );
-              // Center the location marker on the map and zoom the map to level 18.
-              _centerCurrentLocationStreamController.add(18);
-            },
-            child: const Icon(
-              Icons.my_location,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        Positioned(
-          right: 120,
-          bottom: 20,
-          child: FloatingActionButton(
-            onPressed: () {
-              // Automatically center the location marker on the map when location updated until user interact with the map.
-              refresh();
-            },
-            child: const Icon(
-              Icons.refresh,
-              color: Colors.white,
+          child: InkWell(
+            onLongPress: refresh,
+            child: FloatingActionButton(
+              onPressed: () {
+                // Automatically center the location marker on the map when location updated until user interact with the map.
+                setState(
+                  () => _centerOnLocationUpdate = CenterOnLocationUpdate.always,
+                );
+                // Center the location marker on the map and zoom the map to level 18.
+                _centerCurrentLocationStreamController.add(18);
+              },
+              child: const Icon(
+                Icons.my_location,
+                color: Colors.white,
+              ),
             ),
           ),
         ),
       ],
       children: [
-
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'it.eldavo.aqm',
@@ -120,97 +131,81 @@ class _MapPageState extends State<MapPage> with AutomaticKeepAliveClientMixin<Ma
         ),
         CurrentLocationLayer(
           centerCurrentLocationStream:
-          _centerCurrentLocationStreamController.stream,
+              _centerCurrentLocationStreamController.stream,
           centerOnLocationUpdate: _centerOnLocationUpdate,
         ),
-
         MarkerClusterLayerWidget(
-            options: MarkerClusterLayerOptions(
+          options: MarkerClusterLayerOptions(
             spiderfyCircleRadius: 80,
             spiderfySpiralDistanceMultiplier: 2,
             circleSpiralSwitchover: 12,
             maxClusterRadius: 120,
             rotate: true,
             size: const Size(40, 40),
-        anchor: AnchorPos.align(AnchorAlign.center),
-        fitBoundsOptions: const FitBoundsOptions(
-          padding: EdgeInsets.all(50),
-          maxZoom: 15,
-        ),
-        markers: _markers,
-        polygonOptions: const PolygonOptions(
-            borderColor: Colors.blueAccent,
-            color: Colors.black12,
-            borderStrokeWidth: 3),
-        popupOptions: PopupOptions(
-          popupState: PopupState(),
-            popupSnap: PopupSnap.markerTop,
-            popupController: _popupController,
-            popupBuilder: (_, marker) => Container(
-              width: 200,
-              height: 100,
-              color: Colors.white,
-              child: GestureDetector(
-                onTap: () {
-                  // close the popup
-                  _popupController.hideAllPopups();
-                },
-                child: Text(
-                  'Container popup for marker at ${marker.point}',
-                ),
-              ),
-            )),
-        builder: (context, markers) {
-          return Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20.0),
-                color: Colors.blue),
-            child: Center(
-              child: Text(
-                markers.length.toString(),
-                style: const TextStyle(color: Colors.white),
-              ),
+            anchor: AnchorPos.align(AnchorAlign.center),
+            fitBoundsOptions: const FitBoundsOptions(
+              padding: EdgeInsets.all(50),
+              maxZoom: 15,
             ),
-          );
-        },),),
-
+            markers: _markers,
+            polygonOptions: const PolygonOptions(
+                borderColor: Colors.blueAccent,
+                color: Colors.black12,
+                borderStrokeWidth: 3),
+            popupOptions: PopupOptions(
+                popupState: PopupState(),
+                popupSnap: PopupSnap.markerTop,
+                popupController: _popupController,
+                popupBuilder: popupBuilder),
+            builder: (context, markers) {
+              return Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.blue),
+                child: Center(
+                  child: Text(
+                    markers.length.toString(),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
       ],
-
-        );
+    );
   }
-  static List<Marker> latlngtomarkers(List<LatLng> coords){
+
+  static List<Marker> latlngtomarkers(List<LatLng> coords) {
     return coords
         .map((point) => Marker(
-      point: point,
-      width: 60,
-      height: 60,
-      builder: (context) => const Icon(
-        Icons.pin_drop,
-        size: 60,
-        color: Colors.blueAccent,
-      ),
-    ))
+              point: point,
+              width: 60,
+              height: 60,
+              builder: (context) => const Icon(
+                Icons.pin_drop,
+                size: 60,
+                color: Colors.blueAccent,
+              ),
+            ))
         .toList();
   }
 
-  static List<Marker> placetomarkers(List<Place> coords){
+  static List<Marker> placetomarkers(List<Place> coords) {
     return coords
         .map((point) => Marker(
-      point:
-        point.location,
-      width: 60,
-      height: 60,
-      builder: (context) => const Icon(
-        Icons.pin_drop,
-        size: 60,
-        color: Colors.blueAccent,
-      ),
-    ))
+              point: point.location,
+              width: 60,
+              height: 60,
+              builder: (context) => const Icon(
+                Icons.pin_drop,
+                size: 60,
+                color: Colors.blueAccent,
+              ),
+            ))
         .toList();
   }
 
   @override
   bool get wantKeepAlive => true;
-
 }
-
