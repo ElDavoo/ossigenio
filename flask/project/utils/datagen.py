98d41846140c95505f183e7747904b5d3ff70988
David <github@davidepalma.it>
2023-01-06 10:52:44 +0100

    fix fake data generator maybe

--- flask/project/utils/datagen.py
+++ flask/project/utils/datagen.py
@@ -10,26 +10,27 @@ import time
 import threading
 
 
-def generate():
+def generate(rnd_iter):
     # Get all the places
     places = Place.query.all()
     for place in places:
         # Generate a random number of CO2
-        co2 = next(plausible_random(400, 2000))
+        co2 = next(rnd_iter)
         # Save it into the database
-        db.session.add(co2_history(place_id=place.id, co2=co2))
+        db.session.add(co2_history(place_id=place.id, timestamp=time.time(), co2=co2))
         db.session.commit()
 
 
 def run(app):
     print("Starting the CO2 generator")
+    rnd_gen = plausible_random(400, 2000)
     while True:
         # Wait 30 minutes
         time.sleep(1800)
         # Generate the CO2 values
         print("Generating CO2 values")
         with app.app_context():
-            generate()
+            generate(rnd_gen)
 
 
 # Start the thread and make it possible to fork it to the background
