3991ec03b97806d75c5f5f41ef3f41cba051f164
David <github@davidepalma.it>
2023-01-06 22:14:24 +0100

    add prediction chart

--- flutter_app/lib/ui/pages/place_page.dart
+++ flutter_app/lib/ui/pages/place_page.dart
@@ -1,4 +1,6 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/utils/ui.dart';
 
 import '../../managers/account_man.dart';
 
@@ -42,21 +44,125 @@ class _PredictionPlaceState extends State<PredictionPlace> {
       appBar: AppBar(
         title: Text(widget.place.name),
       ),
-      body: FutureBuilder<List<Prediction>>(
-          future: future,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(snapshot.data![index].timestamp.toString()),
-                      subtitle: Text(snapshot.data![index].co2.toString()),
-                    );
-                  });
-            }
-            return const Text('No data');
-          }),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue, Colors.white],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Widget that contains the place's co2level
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Al momento a ${widget.place.name} ci sono ${widget.place.co2Level} ppm di CO2",
+                  style: const TextStyle(fontSize: 40),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Livelli di CO2 previsti per le prossime 24h'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: SizedBox(
+                  height: 300,
+                  child: FutureBuilder<List<Prediction>>(
+                      future: future,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return LineChart(
+                            LineChartData(
+                              lineTouchData: LineTouchData(enabled: false),
+                              gridData: FlGridData(show: false),
+                              titlesData: FlTitlesData(
+                                show: true,
+                                topTitles: AxisTitles(
+                                  sideTitles: SideTitles(showTitles: false),
+                                ),
+                                leftTitles: AxisTitles(
+                                  sideTitles: SideTitles(showTitles: false),
+                                ),
+                                bottomTitles: rightTitles(),
+                              ),
+                              borderData: FlBorderData(show: true),
+                              minX: 0,
+                              maxX: 24,
+                              minY: 400,
+                              lineBarsData: [
+                                LineChartBarData(
+                                  spots: getSpots(snapshot),
+                                  isCurved: true,
+                                  barWidth: 3,
+                                  isStrokeCapRound: true,
+                                  dotData: FlDotData(show: false),
+                                  belowBarData: BarAreaData(show: false),
+                                ),
+                              ],
+                            ),
+                          );
+                        } else if (snapshot.hasError) {
+                          return Text("${snapshot.error}");
+                        }
+                        return UIWidgets.spinText("Sto predicendo il futuro...");
+                      }),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  List<FlSpot> getSpots(AsyncSnapshot<List<Prediction>> snapshot) {
+    var list =
+        snapshot.data!.map((e) => {'t': 0, 'c': e.co2.toDouble()}).toList();
+    // Delete X and make it progressive
+    for (int i = 0; i < list.length; i++) {
+      list[i]['t'] = i.toDouble();
+    }
+    return list
+        .map((e) => FlSpot(e['t']!.toDouble(), e['c']!.toDouble()))
+        .toList();
+  }
+
+  AxisTitles rightTitles() {
+    return AxisTitles(
+      sideTitles: SideTitles(
+        showTitles: true,
+        getTitlesWidget: bottomTitleWidgets,
+      ),
+    );
+  }
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      fontWeight: FontWeight.bold,
+      color: Colors.blueGrey,
+      fontFamily: 'Digital',
+      fontSize: 18,
+    );
+    String text;
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      child: Text(hhmm(value.toInt()), style: style),
+    );
+  }
+
+  String hhmm(int offset) {
+    // Get the current HH:MM
+    var now = DateTime.now();
+    var hour = (now.hour + offset) % 24;
+    var minute = now.minute;
+    return '$hour:$minute';
   }
 }
