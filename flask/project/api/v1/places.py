9a9fed9237d1860391ba7f28bc9abb1cb68ad0ae
David <github@davidepalma.it>
2023-01-06 19:12:16 +0100

    add places api

--- flask/project/api/v1/places.py
+++ flask/project/api/v1/places.py
@@ -20,6 +20,26 @@ from project.models.places import Place, PlaceSchema
 from project.models.co2history import co2_history
 
 
+# Route to get the closest N places to a given location
+@places.route('/places', methods=['POST'])
+class Placs(MethodView):
+    @places.arguments(LatLonSchema)
+    def post(self, args):
+        # Get the closest places
+        placs = Place.query.order_by(Place.location.ST_DistanceSphere(f"POINT({args['lat']} {args['lon']})")).limit(
+            1000)
+        places = []
+        for plc in placs:
+            plc_json = PlaceSchema().dump(plc)
+            last_co2 = co2_history.query.filter_by(place_id=plc.id).order_by(co2_history.timestamp.desc()).first()
+            if last_co2 is not None:
+                plc_json['co2'] = last_co2.co2
+            else:
+                plc_json['co2'] = None
+            places.append(plc_json)
+        return jsonify(places)
+
+
 @places.route('/nearby', methods=['POST'])
 class Nearby(MethodView):
     @places.arguments(LatLonSchema)
