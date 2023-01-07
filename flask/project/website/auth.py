06e2ecdca4a733a80a54798bdfd3a157d5e8b425
AlfaSierra92 <solida.antonio@gmail.com>
2023-01-07 12:34:19 +0100

    Fix missing library

--- flask/project/website/auth.py
+++ flask/project/website/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import sha256
 from project.models.user import Utente
 from project import db
 from flask_login import login_user, login_required, logout_user
